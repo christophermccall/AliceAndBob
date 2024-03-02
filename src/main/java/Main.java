ad406da98bf0225f0c97513887fa3f0763fe7ffd
christophermccall <camccall24@gmail.com>
2024-03-01 19:06:27 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
 public class Main {
-
     public static void main(String[] args ){
         // create a new object of class Main
-
+        Main myObj = new Main();
         // call the compute() method on that new object
+        myObj.compute();
     }
-
     public void compute() {
         // Get input string of name from user
-        
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
         // Create conditional to check name against Alice and Bob
+        if(name.compareTo("Alice")== 0 || name.compareTo("Bob")==0) {
+            System.out.println("Howdy!");
+        } else {
+            System.out.println("You are neither Alice nor Bob.");
+        }
 
         // Print greeting to screen if Alice or Bob are true
         
